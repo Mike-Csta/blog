35919efa53f2fc3cecd8c63b7dd09499db6c24ab
Mike_Csta <mikolaj.chlasta@vimos.eu>
2026-01-18 14:07:33 +0100

    styled navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,50 +1,15 @@
-import { useState } from "react";
-import { AppSidebar } from "./app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import NavigationBar from "./NavigationBar";
 
 type LayoutType = {
   children: React.ReactNode;
 };
 
-const SIDEBAR_STORAGE_KEY = "sidebar-open";
-
-const SIDEBAR_WIDTH_OPEN = "256px";
-const SIDEBAR_WIDTH_COLLAPSED = "48px";
-
 const Layout = ({ children }: LayoutType) => {
-  const isDefaultSidebarOpen = localStorage.getItem(SIDEBAR_STORAGE_KEY);
-
-  const [isSidebarOpen, setIsSidebarOpen] = useState(
-    isDefaultSidebarOpen ?? true,
-  );
-
   return (
-    <SidebarProvider
-      open={isSidebarOpen}
-      onOpenChange={(open) => {
-        localStorage.setItem(SIDEBAR_STORAGE_KEY, open);
-        setIsSidebarOpen(open);
-      }}
-    >
-      <div
-        className="grid h-screen w-full"
-        style={{
-          gridTemplateColumns: isSidebarOpen
-            ? `${SIDEBAR_WIDTH_OPEN} 1fr`
-            : `${SIDEBAR_WIDTH_COLLAPSED} 1fr`,
-          transition: "grid-template-columns 150ms ease-in-out",
-        }}
-      >
-        <AppSidebar />
-        <div className="flex max-h-screen min-w-0 flex-col">
-          {/* <div className="absolute t-0 l-0 text-[700px] rotate-30 text-red-600 opacity-5 z-100">
-            TEST
-          </div> */}
-
-          <main className="flex-1 overflow-auto">{children}</main>
-        </div>
-      </div>
-    </SidebarProvider>
+    <div className="flex h-screen flex-col overflow-hidden">
+      <NavigationBar />
+      <main className="flex-1 overflow-y-auto p-4">{children}</main>
+    </div>
   );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,5 @@
-import Lottie from "lottie-react";
-import anim from "../../../public/anim.json";
-import { ThemeSwitch } from "@/components/ThemeSwitch";
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
-      <div className="h-400 w-full">
-        <Lottie
-          animationData={anim}
-          loop={true}
-          className="absolute top-0 h-full opacity-50"
-        />
-      </div>
-      <ThemeSwitch />
-    </div>
+    <div className="flex h-screen w-full flex-col items-center justify-center"></div>
   );
 }
