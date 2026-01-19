9d282fd1112792fb5fcb9bd76288673723a31291
mike csta <mikolaj.chlasta@vcorrect.eu>
2026-01-19 15:18:38 +0100

    add bentogrid component

---
+++ src/pages/home/BentoGrid.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BentoGrid = () => {
+  return (
+    <section className="w-full flex  items-end justify-between gap-10 py-20 px-15">
+      <div>
+        <div>
+          <h2>Obszary badań</h2>
+          <p>
+            Gdzie aktualnie szukam lepszych rozwiązań niż &quot;standard
+            rynkowy;.
+          </p>
+        </div>
+      </div>
+      <div className="font-mono">STATUS: AKTYWNY ROZWÓJ</div>
+    </section>
+  );
+};
+export default BentoGrid;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
+import BentoGrid from "./BentoGrid";
 import Hero from "./Hero";
 
 export default function Home() {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <Hero />
+      <BentoGrid />
     </div>
   );
 }
